0251e1e4faf47d7074db8bf44132b114b2e22e78
Laurent Huang <laurenthuang@hotmail.fr>
2019-08-21 18:30:13 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,23 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo);
+        System.out.println("\t---------------------------------------------------------------------------------");
+        System.out.println("\t Hello I'm Duke\n\t What can I do for you ?");
+        System.out.println("\t---------------------------------------------------------------------------------");
+
+        Scanner sc = new Scanner(System.in);
+        String user = sc.nextLine();
+        while (!user.equals("bye")){
+            System.out.println("");
+            System.out.println("\t---------------------------------------------------------------------------------");
+            System.out.println("\t "+ user);
+            System.out.println("\t---------------------------------------------------------------------------------");
+            user=sc.nextLine();
+        }
+        System.out.println("\t---------------------------------------------------------------------------------");
+        System.out.println("\t Bye. Hope to see you again soon!");
+        System.out.println("\t---------------------------------------------------------------------------------");
+
     }
 }
